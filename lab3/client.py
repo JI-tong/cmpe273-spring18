dca397a583c02fc7d2d22f149c5474c59bbde20c
Ji.Tong <ton9eric1@gmail.com>
2018-03-02 21:13:35 -0800

    lab3

--- lab3/client.py
+++ lab3/client.py
@@ -1,18 +1,50 @@
 import zmq
 import sys
 
+def send_message(name, message):
+    data = {'username' : name,
+            'message' : message}
+    return data
+def has_message(poller, display):
+    events = poller.poll()
+    return display in events
+    #events = dict(poller.poll(3000))
+    #return events.get(sock) == zmq.POLLIN
+
+
 # ZeroMQ Context
 context = zmq.Context()
 
 # TODO: change this to PUB pattern.
 # Define the socket using the "Context"
+display = context.socket(zmq.SUB)
+display.setsockopt_string(zmq.SUBSCRIBE,'')
+display.connect("tcp://127.0.0.1:5677")
+
+
 sock = context.socket(zmq.REQ)
 sock.connect("tcp://127.0.0.1:5678")
 
-# Send a "message" using the socket
-msg = " ".join(sys.argv[1:])
-print("[client]:" + msg)
-sock.send_string(msg)
 
-msg = sock.recv()
-print("[received]:" + msg.decode())
+#poller = zmq.Poller().register(sock, zmq.POLLIN)
+
+msg = " ".join(sys.argv[1:])
+print("user [{}] Connected to the chat server".format(msg))
+name = msg
+#sock.send_string(msg)
+tit = "[{}] > ".format(name)
+
+poller = zmq.Poller()
+poller = poller.register(sock, zmq.POLLIN)
+
+
+while 1:
+    message = input(tit)
+    data = send_message(name, message)
+    sock.send_json(data)
+
+    sock.recv_string()
+    data = display.recv_json()
+    username, message = data['username'], data['message']
+    if(username != name):
+        print("[{}]: {}".format(username, message))
--- lab3/server.py
+++ lab3/server.py
@@ -7,10 +7,28 @@ context = zmq.Context()
 sock = context.socket(zmq.REP)
 sock.bind("tcp://127.0.0.1:5678")
 
+display = context.socket(zmq.PUB)
+display.bind("tcp://127.0.0.1:5677")
+
+
+def get_message(sock):
+    data = sock.recv_json()
+    sock.send_string("received")
+    username = data['username']
+    message = data['message']
+    return username,message
+
 # Run a simple "Echo" server
 while True:
-    message = sock.recv()
+    username, message = get_message(sock)
+    data = {'username' : username,
+            'message' : message}
+    display.send_json(data)
+
+
+    '''
     message = message.decode()
     message = message[::-1]
     sock.send_string("Echo: " + message)
     print("[Server] Echo: " + message)
+    '''
